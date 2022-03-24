87077bd99b2143718d664de28d213b3e93681748
Ramon Marques <ramon.junior7@enova.educacao.ba.gov.br>
2022-03-24 20:43:41 -0300

    change(database): adicionadas configurações de database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express from "express";
+
 import { router } from "./routes";
+
+import "./database";
+
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
-
 
 app.listen(3333, () => console.log("Server is running..."));
